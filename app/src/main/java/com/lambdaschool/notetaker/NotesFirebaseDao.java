0c7d32757195d520ca652a2c6c011b8175af7f0b
VivekV95 <vivekv210@gmail.com>
2019-05-06 15:11:58 -0500

    Replaced userid constant with integer regex in dao

--- app/src/main/java/com/lambdaschool/notetaker/NotesFirebaseDao.java
+++ app/src/main/java/com/lambdaschool/notetaker/NotesFirebaseDao.java
@@ -13,9 +13,9 @@ public class NotesFirebaseDao {
     private static final String NOTES = "notes/";
     private static final String URL_ENDING = ".json";
 
-    private static final String USER_INFO = BASE_URL + USER_ID + URL_ENDING;
-    private static final String USER_NOTES = BASE_URL + USER_ID + NOTES + URL_ENDING;
-    private static final String USER_SPECIFIC_NOTE = BASE_URL + USER_ID + NOTES + "%s/" + URL_ENDING; //use string.format to add id
+    private static final String USER_INFO = BASE_URL + "%d/" + URL_ENDING;
+    private static final String USER_NOTES = BASE_URL + "%d/" + NOTES + URL_ENDING;
+    private static final String USER_SPECIFIC_NOTE = BASE_URL + "%d/" + NOTES + "%s/" + URL_ENDING; //use string.format to add id
 
     public static ArrayList<Note> getNotes() {
         ArrayList<Note> notes = new ArrayList<>();
